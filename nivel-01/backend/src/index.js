c2247ac06c4b3785817ef8f422f9cea054277c8a
fernando-meira <thomwork@outlook.com>
2020-11-29 21:41:38 -0300

    feat: 🎸 Added route delete and post in projects

--- nivel-01/backend/src/index.js
+++ nivel-01/backend/src/index.js
@@ -14,15 +14,57 @@ app.post("/projects", (request, response) => {
   const { title, owner } = request.body;
 
   const project = {
+    owner,
+    title,
     id: uuidv4(),
+  };
+
+  projects.push(project);
+
+  return response.status(201).json(project);
+});
+
+app.put("/projects/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response
+      .status(404)
+      .json({ success: false, error: "Project not found." });
+  }
+
+  const project = {
+    id,
     title,
     owner,
   };
 
-  projects.push(project);
-  console.log(project);
+  projects[projectIndex] = project;
 
   return response.status(200).json(project);
+});
+
+app.delete("/projects/:id", (request, response) => {
+  const { id } = request.params;
+
+  console.log(id);
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  console.log(projectIndex);
+
+  if (projectIndex < 0) {
+    return response
+      .status(400)
+      .json({ success: false, error: "Project not found." });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(200).send();
 });
 
 app.listen(3333, () => {
