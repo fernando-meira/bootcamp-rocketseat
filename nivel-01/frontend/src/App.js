3283b87f27417030f84d6c9c4eb039879c25011b
fernando-meira <thomwork@outlook.com>
2020-12-13 18:13:27 -0300

    feat: 🎸 Iniciado conceitos de ReactJS

---
+++ nivel-01/frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import { Header } from "./components"
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Header title="HomePage" />
+      <Header title="Projects" />
+      <Header />
+    </>
+  )
+}
+
+export default App
