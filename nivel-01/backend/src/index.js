39fea92a6ffe69d05fab56f2932e347a4d556b61
fernando-meira <thomwork@outlook.com>
2020-12-19 23:31:49 -0300

    Adicionado cors

--- nivel-01/backend/src/index.js
+++ nivel-01/backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const { v4: uuidv4, validate: uuidValidate } = require("uuid");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const projects = [];
 
