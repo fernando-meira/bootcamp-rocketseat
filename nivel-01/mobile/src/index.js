090ce9a60c4a9d2945fe388468c96cc76a1ec6ad
fernando-meira <thomwork@outlook.com>
2020-12-31 16:08:27 -0300

    feat: 🎸 Diferenças do ReactJS
    Nessa aula aprendemos algumas diferenças entre o React native e o
    ReactJS, como por exemplo a não utilização de tags HTML para componentes
    e sim componentes próprios do react native como View e Text, estilização
    utilizando o StyleSheet, e como trabalhar com a StatusBar.

--- nivel-01/mobile/src/index.js
+++ nivel-01/mobile/src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, StyleSheet, StatusBar} from 'react-native';
 
 function App() {
-  return <View />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello World</Text>
+      </View>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#7159c1',
+  },
+  title: {
+    fontSize: 32,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
 
 export default App;
