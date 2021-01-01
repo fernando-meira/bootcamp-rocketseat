c079c046078817e44e2da40cfe3c064125b34e51
fernando-meira <thomwork@outlook.com>
2021-01-01 02:48:06 -0300

    feat: 🎸 Criando novos projetos
    Nessa aula criamos a funcionalidade para criar novos projetos na nossa
    lista de projetos, vimos alternativas para criar um botão customizável,
    aprendemos a chamar uma função ao pressionar o botão e para finalizar
    enviamos uma requisição ao backend para criar o projeto e atualizar a
    lista.

--- nivel-01/mobile/src/index.js
+++ nivel-01/mobile/src/index.js
@@ -1,16 +1,33 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 import {
   Text,
   FlatList,
   StatusBar,
   StyleSheet,
   SafeAreaView,
+  TouchableOpacity,
 } from 'react-native';
 
 import api from './services/api';
 
 function App() {
   const [repositories, setRepositories] = useState([]);
+
+  const handleAddRepository = useCallback(async () => {
+    try {
+      const {data} = await api.post('repositories', {
+        title: `New project ${Date.now()}`,
+        url: 'https://github.com/fernando-meira',
+        techs: ['Node', 'Express', 'TypeScript'],
+      });
+
+      const newRepository = data;
+
+      setRepositories([...repositories, newRepository]);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [repositories]);
 
   useEffect(() => {
     api.get('repositories').then((response) => {
@@ -30,6 +47,13 @@ function App() {
             <Text style={styles.title}>{repository.title}</Text>
           )}
         />
+
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddRepository}>
+          <Text style={styles.textButton}>Adicionar Projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
@@ -41,8 +65,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#7159c1',
   },
   title: {
-    fontSize: 32,
+    fontSize: 24,
     color: '#fff',
+    fontWeight: 'bold',
+  },
+  button: {
+    margin: 20,
+    padding: 20,
+    borderRadius: 4,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF',
+  },
+  textButton: {
+    fontSize: 24,
     fontWeight: 'bold',
   },
 });
