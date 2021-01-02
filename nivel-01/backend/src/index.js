071d8ec43b75512859f87bd88bd52d746815f2c9
fernando-meira <thomwork@outlook.com>
2021-01-02 13:33:04 -0300

    feat: 🎸 Added like route

--- nivel-01/backend/src/index.js
+++ nivel-01/backend/src/index.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const { v4: uuidv4, validate: uuidValidate } = require("uuid");
+const { isUuid } = require("uuidv4");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const projects = [];
+const repositories = [];
 
-function validateProjectId(request, response, next) {
+function validateRepositoryId(request, response, next) {
   const { id } = request.params;
 
   const isUuid = uuidValidate(id);
 
   if (!isUuid) {
-    return response.status(400).json({ error: "Invalid project ID." });
+    return response.status(400).json({ error: "Invalid repository ID." });
   }
 
   return next();
@@ -33,71 +34,91 @@ function logRequest(request, response, next) {
 }
 
 app.use(logRequest);
-app.use("/projects/:id", validateProjectId);
+app.use("/repositories/:id", validateRepositoryId);
 
-app.get("/projects", (request, response) => {
+app.get("/repositories", (request, response) => {
   const { title } = request.query;
 
   const result = title
-    ? projects.filter((project) => project.title.includes(title))
-    : projects;
+    ? repositories.filter((repository) => repository.title.includes(title))
+    : repositories;
 
   return response.status(200).json(result);
 });
 
-app.post("/projects", (request, response) => {
-  const { title, owner } = request.body;
+app.post("/repositories", (request, response) => {
+  const { title, url, techs, likes } = request.body;
 
-  const project = {
+  const repository = {
+    url,
     title,
-    owner,
+    techs,
     id: uuidv4(),
+    likes: likes ? likes : 0,
   };
 
-  projects.push(project);
+  repositories.push(repository);
 
-  return response.status(201).json(project);
+  return response.status(201).json(repository);
 });
 
-app.put("/projects/:id", (request, response) => {
+app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const { title, owner } = request.body;
 
-  const projectIndex = projects.findIndex((project) => project.id === id);
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
 
-  if (projectIndex < 0) {
+  if (repositoryIndex < 0) {
     return response
       .status(404)
-      .json({ success: false, error: "Project not found" });
+      .json({ success: false, error: "repository not found" });
   }
 
-  const project = {
+  const repository = {
     title,
     owner,
     id,
   };
 
-  projects[projectIndex] = project;
+  repositories[repositoryIndex] = repository;
 
-  return response.status(200).json(project);
+  return response.status(200).json(repository);
 });
 
-app.delete("/projects/:id", (request, response) => {
+app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  const projectIndex = projects.findIndex((project) => project.id === id);
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
 
-  if (projectIndex < 0) {
+  if (repositoryIndex < 0) {
     return response
       .status(404)
-      .json({ success: false, error: "Project not found." });
+      .json({ success: false, error: "repository not found." });
   }
 
-  projects.splice(projectIndex, 1);
+  repositories.splice(repositoryIndex, 1);
 
-  return response
-    .status(204)
-    .json({ success: true, message: "Deleted project." });
+  response.status(204).json({ message: "Deleted reposotory." });
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  const uuidValidate = isUuid(id);
+
+  if (!uuidValidate) {
+    return response.status(400).json({ error: "Invalid project id." });
+  }
+
+  const repository = repositories.find((repository) => repository.id === id);
+
+  repository.likes += 1;
+
+  return response.status(200).json(repository);
 });
 
 app.listen(3333, () => {
