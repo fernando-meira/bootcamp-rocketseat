e595a7b0411a73a98bc2978f034fe026517f3308
fernando-meira <thomwork@outlook.com>
2021-01-03 13:30:56 -0300

    feat: 🎸 Primeiros conceitos de Typescript
    Nível 1 Bootcamp GoStack concluído.

--- nivel-01/typescript/src/routes.ts
+++ nivel-01/typescript/src/routes.ts
@@ -1,5 +1,21 @@
 import { Response, Request } from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    name: "Fernando", 
+    email: 'fernando.santos121@fatec.itapetininga.edu.br', 
+    password: "12345",
+    techs: [
+      "NodeJS",
+      "ReactJS",
+      "TypeScript",
+      {title: "Javascript", experience: 100},
+      {title: "NodeJS", experience: 65}
+    ]
+  });
+
+  console.log(user);
+  
   return response.json({ message: "Hello World"})
 };
